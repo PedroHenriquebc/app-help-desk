05f9666ef51e457c33c8b340af6a53e439e0edc1
Pedro Henrique <pedrohbc96@gmail.com>
2022-11-30 21:18:23 -0300

    Adiciona registro de chamados

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,5 +1,22 @@
 <?php
 
-    
+    //trabalhando na montagem do texto
+    $titulo = str_replace("#", "-", $_POST["titulo"]);
+    $categoria = str_replace("#", "-", $_POST["categoria"]);
+    $descricao = str_replace("#", "-", $_POST["descricao"]);
+
+    $texto = $titulo . "#" . $categoria . "#" . $descricao . PHP_EOL;
+
+    //abrindo arquivo
+    $arquivo = fopen("arquivo.txt", "a");
+
+    //escrevendo o texto
+    fwrite($arquivo, $texto);
+
+    //fechando o arquivo
+    fclose($arquivo);
+
+    //redirecionando para abrir_chamado.php
+    header("Location: abrir_chamado.php");
 
 ?>
