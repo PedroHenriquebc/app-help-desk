589b70d6c11b68c6031bb866f7e4925a4ec23a89
Pedro Henrique <pedrohbc96@gmail.com>
2022-12-21 23:12:50 -0300

    Implementa consulta de chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,5 +1,22 @@
 <?php require_once "validador_acesso.php" ?>
 
+<?php
+
+  //chamados
+  $chamados = array();
+
+  //abrindo arquivo
+  $arquivo = fopen("arquivo.txt", "r");
+
+  //enquanto houver registros (linhas) a serem recuperados
+  while(!feof($arquivo)) { //testa pelo fim de um arquivo
+    $registro = fgets($arquivo); //recupera uma linha do arquivo
+    $chamados[] = $registro;
+  }
+
+  fclose($arquivo);
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -41,23 +58,27 @@
             
             <div class="card-body">
               
+            <?php foreach($chamados as $chamado) { ?>
+
+              <?php 
+              
+                $chamado_dados = explode("#", $chamado);
+                if(count($chamado_dados) < 3) {
+                  continue;
+                }
+
+              ?>
+
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                  <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                  <p class="card-text"><?= $chamado_dados[2] ?></p>
 
                 </div>
               </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+            <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
